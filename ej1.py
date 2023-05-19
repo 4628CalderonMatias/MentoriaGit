d8d9d6936d374a677c27e26e25bccaa52ea80f3d
4628CalderonMatias <132401739+4628CalderonMatias@users.noreply.github.com>
2023-05-19 17:51:30 -0300

    agregando mi primer archivo

---
+++ ej1.py
@@ -0,0 +1,7 @@
+# 1- Escribi un programa que pida al usuario su edad y muestre en pantalla si es mayor de edad (18 años o mas) o no
+
+usuario = int(input("31."))
+if usuario >=18:
+    print('Eres Mayor de Edad.')
+else:
+    print('No eres mayor de Edad, eres Menor.')
